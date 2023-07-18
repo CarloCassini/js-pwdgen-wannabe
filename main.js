574b02354337916e1d302c00307d8eaaa08c1fc7
CarloCassini <c.cassini87@gmail.com>
2023-07-18 16:07:59 +0200

    add strafanti

--- main.js
+++ main.js
@@ -18,9 +18,23 @@ const MessageToDisplay = `
 <h1> cognome <span>${surname}</span> </h1>
 <h1> colore preferito <span>${color}</span> </h1>
 <h1> numero <span>${index}</span> </h1>
+
+<p> tutto vicino: </p>
+
+${firstName}${surname}${color}${index}
+
+<p> mo esagero: </p>
+<img
+src="https://learn.microsoft.com/it-it/shows/hello-world/media/helloworld_383x215.png"
+alt=""
+/>
 `;
 
 // collegamento per trascrivere il risultato sul div
 //  "id=message-to-read"
 
 document.getElementById("message-to-read").innerHTML = MessageToDisplay;
+
+// stampa su console
+console.log(firstName + surname + color + index);
+console.log(firstName + " " + surname + " " + color + " " + index);
